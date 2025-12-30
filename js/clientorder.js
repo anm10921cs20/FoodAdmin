960fe1c921f73b544a6a124785ba16b3fe223c4f
mahavishnu <mahanvishnu3@gamil.com>
2025-12-31 00:58:32 +0530

    update

--- js/clientorder.js
+++ js/clientorder.js
@@ -4,6 +4,7 @@ db.ref('userorder').get('value').then((snapchat) => {
     const userOrder = snapchat.val();
     const ConvertUserData = Object.entries(userOrder)
     const todayArray = []
+    const todayUid = []
     ConvertUserData.forEach((userdata, index) => {
         const userid = userdata[0];
 
@@ -14,8 +15,10 @@ db.ref('userorder').get('value').then((snapchat) => {
             const pushData = itemsget;
             const pushDataid = pushData[0]
             const pushOrderData = pushData[1]
+            
+            
 
-            addOrder(pushOrderData, todayArray)
+            addOrder(pushOrderData, todayArray, pushDataid, todayUid)
 
 
 
@@ -26,13 +29,14 @@ db.ref('userorder').get('value').then((snapchat) => {
 
     const dateFilter = todayArray.filter((today, index) => today.date === new Date().toLocaleDateString())
     dateFilter.forEach((todaypro, index) => {
-        todayOrder(todaypro, index)
+        todayOrder(todaypro, index, todayUid)
 
     })
     const dateSort = todayArray.reverse()
     dateSort.forEach((items, index) => {
-        oldOrder(items, index)
+        oldOrder(items, index, todayUid)
     })
+
 
 
 
@@ -45,9 +49,11 @@ db.ref('userorder').get('value').then((snapchat) => {
 });
 
 // add order
-function addOrder(pushOrderData, todayArray) {
+function addOrder(pushOrderData, todayArray, pushDataid, todayUid) {
     const todayOrder = pushOrderData;
     todayArray.push(todayOrder)
+    todayUid.push(pushDataid);
+    
 
 
 }
@@ -85,26 +91,204 @@ function todayOrder(todaypro, index) {
                         </td>
                         <td class="deliveryStatus">deliveryStatus</td></tr>
 
-                <div class="offcanvas offcanvas-start" id="orderdet${index}">
-    
+<div class="offcanvas offcanvas-start" id="orderdet${index}">
+ <div class="orderitem-header">
+            <div class="orderiddata">
+                <div class="orderuniqid">
+                    UserUid #${todaypro.useruid}
+                </div>
+                <div class="item-count">
+                ${todaypro.username} | ${todaypro.food.length} Items, Rs.${todaypro.total.ToPay}
+                </div>
+            </div>
+            <div class="close-icon">
+                <i class="fas fa-close" type="button" data-bs-dismiss="offcanvas"></i>
+            </div>
+        </div>
+  <div class="order-address-container">
+                <div class="fromaddress">
+                    <div class="formicon"><i class="fas fa-location-dot"></i></div>
+                    <div class="officeaddress-container">
+                        <div class="officename">Mahan Food</div>
+                        <div class="officeaddress">Kn Pettai</div>
+                    </div>
+                </div>
+                <div class="toaddress">
+                    <div class="toicon"><i class="fas fa-home"></i></div>
+                    <div class="homeaddress-container">
+                        <div class="homename">Home</div>
+                        <div class="homeaddress">${todaypro.address}</div>
+                    </div>
+                </div>
+                <div class="travel-line"></div>
+            </div>
+            <div class="checkout-summary-container">
+                <div class="checkout-summary-content">
+                    <div class="checkout-summary-content-det">
+                        Bill Summary
+                    </div>
+                </div>
+                <div class="line">
+                </div>
+                <div class="checkout-summary-price-container">
+                    <div class="checkout-summary-price">
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Item Price</div>
+                            <div class="payamt" id="itemPrice">Rs.${todaypro.total.itemPrice}</div>
+                        </div>
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Delivery Charge</div>
+                            <div class="payamt" id="deliveryCharge">Rs.${todaypro.total.deliveryCharge}</div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="checkout-summary-price-container">
+                    <div class="checkout-summary-price">
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Platform Fee</div>
+                            <div class="payamt" id="platFormFee">Rs.${todaypro.total.platFormFee}</div>
+                        </div>
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Gst & Other Charges</div>
+                            <div class="payamt" id="gstCharges">Rs.${todaypro.total.gstCalculation}</div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="topay">
+                    <div class="checkout-summary-topay-amt">
+                        <div class="Topaycashname">Paid</div>
+                        <div class="Topayamt" id="toPay">Rs.${todaypro.total.ToPay}</div>
+                    </div>
+                </div>
+
+            </div>
 </div>
                         
     `;
+
+
+
+
+
+
+
+
+
+
     const todayItems = document.getElementsByClassName('today-items')[0];
     todayItems.appendChild(tr)
+
+    const offCanavs = document.getElementById(`orderdet${index}`);
+
+
+
+    const foodContainer = todaypro.food;
+    foodContainer.forEach((food, id) => {
+        const addfoodContainer = document.createElement('div')
+        addfoodContainer.innerHTML = `
+                 <div class="order-main">
+            <div class="order-items-data">
+                <div class="order-items-img">
+                    <img src="${food.img}" alt="${food.name}" width="40px" >
+                    </div>
+                    <div class="order-items-detail">
+                        <div class="productname">${food.name}</div>
+                        <div class="product-price-container">
+                            <div class="product-price">
+                            Rs.${parseInt(food.price.slice(3)) / food.quantity}
+                            </div>
+                            <div class="product-count-price">
+                            ${food.price}
+                            </div>
+                        </div>
+                       
+                    </div>
+                     <div class="product-qty-container">Qty:${food.quantity}</div>
+                </div>
+            </div>
+
+                ` ;
+        offCanavs.appendChild(addfoodContainer);
+        offCanavs.style.width = "100%"
+        offCanavs.style.overflowY = "scroll";
+
+
+
+
+
+
+    })
+
+    if (todaypro.isOrderStatus == false) {
+        const divIsOrderStatus = document.createElement('div');
+        divIsOrderStatus.className = "isorder-status";
+        divIsOrderStatus.innerHTML = `
+             <div class="order-status-text">Order Process
+            <i class="fa-solid fa-stopwatch"></i>
+             </div>
+             <div class="radio">
+             <input type="checkbox" class="deliverycomplete" name="checkbox" onchange="update()" > Click To Delivery Success
+             <div>
+             `;
+        divIsOrderStatus.style.color = "#e24141ff"
+        offCanavs.appendChild(divIsOrderStatus)
+    }
+    else {
+        const divIsOrderStatus = document.createElement('div');
+        divIsOrderStatus.className = "isorder-status";
+        divIsOrderStatus.innerHTML = `
+             <div class="order-status-text">Order Delivered
+             <i class="fa-solid fa-circle-check"></i></i>
+             </div>
+             `;
+        divIsOrderStatus.style.color = "#30a76fff"
+        offCanavs.appendChild(divIsOrderStatus)
+    }
+
+
+    function update() {
+        console.log('hii');
+
+    }
+
+
+
+
+
+
+
+
+
+
+
     const total = document.getElementById('todaycount')
     var todayTotalOrder = todayItems.children.length;
     total.innerText = todayTotalOrder;
 
-     var totalValue = 0;
-   const totalAmtElement = document.querySelectorAll('.totalamt');
-   totalAmtElement.forEach((totalElement) => {
-    const totalAmtShow = totalElement.innerHTML.slice(3); 
-     const totalAmt = parseInt(totalAmtShow);
-      totalValue = totalValue + totalAmt;
-   })
-   const totalitem = document.getElementById('total');
-   totalitem.innerText = "Rs."+ totalValue
+    var totalValue = 0;
+    const totalAmtElement = document.querySelectorAll('.totalamt');
+    totalAmtElement.forEach((totalElement) => {
+        const totalAmtShow = totalElement.innerHTML.slice(3);
+        const totalAmt = parseInt(totalAmtShow);
+        totalValue = totalValue + totalAmt;
+    })
+
+
+
+
+
+
+
+
+
+
+
+    const totalitem = document.getElementById('total');
+    totalitem.innerText = "Rs." + totalValue
 
 
     const deliveryStatus = document.getElementsByClassName('deliveryStatus')[index];
@@ -122,9 +306,12 @@ function todayOrder(todaypro, index) {
 
 }
 
+
+
+
 // old
 
-function oldOrder(items, index) {
+function oldOrder(items, index, uid) {
     const tr = document.createElement('tbody');
     const todayTime = items.time;
     const zeroAdd = (data) => {
@@ -145,7 +332,7 @@ function oldOrder(items, index) {
 
 
     tr.innerHTML = `
-    <tr class="tr" data-bs-toggle="offcanvas" data-bs-target ="#orderdet${index}"><td>${index + 1}</td>
+    <tr class="tr" data-bs-toggle="offcanvas" data-bs-target ="#orderdet1${index}"><td>${index + 1}</td>
                         <td>${items.date}</td>
                         <td>${normalTime}:${minutes}:${seconds} ${median}</td>
                         <td>${items.username}</td>
@@ -155,32 +342,215 @@ function oldOrder(items, index) {
                         </td>
                         <td class="deliveryStatus1"></td></tr>
 
-                <div class="offcanvas offcanvas-start" id="orderdet${index}">
-    
+<div class="offcanvas offcanvas-start" id="orderdet1${index}">
+<div class="orderitem-header">
+            <div class="orderiddata">
+                <div class="orderuniqid">
+                    UserUid #${items.useruid}
+                </div>
+                <div class="item-count">
+                ${items.username} | ${items.food.length} Items, Rs.${items.total.ToPay}
+                </div>
+            </div>
+            <div class="close-icon">
+                <i class="fas fa-close" type="button" data-bs-dismiss="offcanvas"></i>
+            </div>
+        </div>
+  <div class="order-address-container">
+                <div class="fromaddress">
+                    <div class="formicon"><i class="fas fa-location-dot"></i></div>
+                    <div class="officeaddress-container">
+                        <div class="officename">Mahan Food</div>
+                        <div class="officeaddress">Kn Pettai</div>
+                    </div>
+                </div>
+                <div class="toaddress">
+                    <div class="toicon"><i class="fas fa-home"></i></div>
+                    <div class="homeaddress-container">
+                        <div class="homename">Home</div>
+                        <div class="homeaddress">${items.address}</div>
+                    </div>
+                </div>
+                <div class="travel-line"></div>
+            </div>
+            <div class="checkout-summary-container">
+                <div class="checkout-summary-content">
+                    <div class="checkout-summary-content-det">
+                        Bill Summary
+                    </div>
+                </div>
+                <div class="line">
+                </div>
+                <div class="checkout-summary-price-container">
+                    <div class="checkout-summary-price">
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Item Price</div>
+                            <div class="payamt" id="itemPrice">Rs.${items.total.itemPrice}</div>
+                        </div>
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Delivery Charge</div>
+                            <div class="payamt" id="deliveryCharge">Rs.${items.total.deliveryCharge}</div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="checkout-summary-price-container">
+                    <div class="checkout-summary-price">
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Platform Fee</div>
+                            <div class="payamt" id="platFormFee">Rs.${items.total.platFormFee}</div>
+                        </div>
+                        <div class="checkout-summary-pay-amt">
+                            <div class="payname">Gst & Other Charges</div>
+                            <div class="payamt" id="gstCharges">Rs.${items.total.gstCalculation}</div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="topay">
+                    <div class="checkout-summary-topay-amt">
+                        <div class="Topaycashname">Paid</div>
+                        <div class="Topayamt" id="toPay">Rs.${items.total.ToPay}</div>
+                    </div>
+                </div>
+
+            </div>    
 </div>
                         
     `;
     const todayItems = document.getElementsByClassName('old-items')[0];
     todayItems.appendChild(tr)
+
+
+    const offCanavs = document.getElementById(`orderdet1${index}`);
+
+
+
+    const foodContainer = items.food;
+    foodContainer.forEach((food, id) => {
+        const addfoodContainer = document.createElement('div')
+        addfoodContainer.innerHTML = `
+                 <div class="order-main">
+            <div class="order-items-data">
+                <div class="order-items-img">
+                    <img src="${food.img}" alt="${food.name}" width="40px" >
+                    </div>
+                    <div class="order-items-detail">
+                        <div class="productname">${food.name}</div>
+                        <div class="product-price-container">
+                            <div class="product-price">
+                            Rs.${parseInt(food.price.slice(3)) / food.quantity}
+                            </div>
+                            <div class="product-count-price">
+                            ${food.price}
+                            </div>
+                        </div>
+                       
+                    </div>
+                     <div class="product-qty-container">Qty:${food.quantity}</div>
+                </div>
+            </div>
+
+                ` ;
+        offCanavs.appendChild(addfoodContainer);
+        offCanavs.style.width = "100%"
+        offCanavs.style.overflowY = "scroll";
+
+
+
+
+
+
+    })
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     const totalOrder = document.getElementById('todaycount1')
     var todayTotalOrder = todayItems.children.length;
     totalOrder.innerText = todayTotalOrder;
-   var totalValue = 0;
-   const totalAmtElement = document.querySelectorAll('.totalamt');
-   totalAmtElement.forEach((totalElement) => {
-    const totalAmtShow = totalElement.innerHTML.slice(3); 
-     const totalAmt = parseInt(totalAmtShow);
-      totalValue = totalValue + totalAmt;
-   })
-  
-   
+    var totalValue = 0;
+    const totalAmtElement = document.querySelectorAll('.totalamt');
+    totalAmtElement.forEach((totalElement) => {
+        const totalAmtShow = totalElement.innerHTML.slice(3);
+        const totalAmt = parseInt(totalAmtShow);
+        totalValue = totalValue + totalAmt;
+    })
 
 
-  
-   const totalitem = document.getElementById('total1');
-   totalitem.innerText = "Rs."+ totalValue
-   
-    
+
+
+
+    const totalitem = document.getElementById('total1');
+    totalitem.innerText = "Rs." + totalValue
+
+
+   if (items.isOrderStatus == false) {
+        const divIsOrderStatus = document.createElement('div');
+        divIsOrderStatus.className = "isorder-status";
+        divIsOrderStatus.innerHTML = `
+             <div class="order-status-text">Order Process
+            <i class="fa-solid fa-stopwatch"></i>
+             </div>
+             <div class="radio">
+             <input type="checkbox" class="deliverycompletes" name="checkbox" > Click To Delivery Success
+             <div>
+             `;
+        divIsOrderStatus.style.color = "#e24141ff"
+        offCanavs.appendChild(divIsOrderStatus)
+
+
+        const deliveryCompleteElement = document.querySelectorAll('.deliverycompletes');
+        
+        
+        deliveryCompleteElement.forEach((value, index) => {
+            value.addEventListener('click', () => {
+                ClickToDeliverd(uid, index, items)
+                
+            })
+            
+        })
+        
+            
+
+       
+    }
+    else {
+        const divIsOrderStatus = document.createElement('div');
+        divIsOrderStatus.className = "isorder-status";
+        divIsOrderStatus.innerHTML = `
+             <div class="order-status-text">Order Delivered
+             <i class="fa-solid fa-circle-check"></i></i>
+             </div>
+             `;
+        divIsOrderStatus.style.color = "#30a76fff"
+        offCanavs.appendChild(divIsOrderStatus)
+    }
+
 
 
 
@@ -196,4 +566,22 @@ function oldOrder(items, index) {
     }
 
 
+}
+
+
+
+
+function ClickToDeliverd(uid, index, items)
+{
+    console.log(uid[index]);
+    
+    db.ref('userorder/' + items.username + items.useruid + "/" + uid[1]).get('value').then((snapchat) => {
+        console.log(snapchat.val());
+        
+    }).catch((err) => {
+        console.log(err);
+        
+    })
+    
+    
 }
